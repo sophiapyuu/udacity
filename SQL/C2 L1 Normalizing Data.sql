2b2758e986ddcab267d72995f1fb816b418bced5
yupia <sophiapyu@gmail.com>
2021-08-23 11:45:13 -0700

    Start of Course 2: L1 Normalizing Data

--- SQL/C2 L1 Normalizing Data.sql
+++ SQL/C2 L1 Normalizing Data.sql
@@ -1,5 +1,24 @@
 --Normalizing Data
 
---Concepts
+--Concepts 4
+--1.
+1000	Alice	HR
+1000	Alice	Sales
+for primary key column use emp_number + department
+--2
+t_id	title	category1
+10	Everything wrong with the world	Editorial
+10	Everything wrong with the world	Clickbait
+11	You won't believe this!	Clickbait
+00	Welcome to our blog           Home
+for primary key column use post_id and category1
 
---1
+--Concepts 7
+--1. wrong
+
+date	employee_id		expense_amount	categories
+2015-05-12	5		25.48	Food
+2015-05-12	5		25.48	Travel
+2015-05-15	42		40	Taxi
+
+make the data long from wide. we can remove employee_name since we have employee_id.  
