a96d055b417fa865dd66ba487ef767975950cc52
yupia <sophiapyu@gmail.com>
2021-08-26 07:57:53 -0700

    C2: L3

---
+++ SQL/C2 L3 Data Manipulation Language DML.sql
@@ -0,0 +1,22 @@
+--DML
+
+--Concepts 2
+
+INSERT INTO "books" ("book_title") VALUES
+("Bourne Identity"),
+("Matrix"),
+("Twilight")
+
+--Concepts 5
+
+1. create table
+2. insert into table
+
+CREATE TABLE "people" (
+  "person_id" SERIAL,
+  "first_name"  VARCHAR,
+  "last_name" VARCHAR
+);
+
+INSERT INTO "people" ("first_name", "last_name")
+  SELECT "first_name", "last_name" FROM "denormalized_people";
